17eb1a6f755f5ecfe22386ec9e686bcc005b1236
egorovmikhail <egorovmikhail@>
2024-06-15 15:38:13 +0300

    KS-1-2 create commit

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOforders: Int = 75
+    val textOfgratitude: String = "Спасибо за покупку"
+//    val numberOfemployees: Int = 2000
+
+    println(numberOforders)
+    println(textOfgratitude)
+//    println(numberOfemployees)
+
+    val numberOfemployees: Int = 1999
+    println(numberOfemployees)
+}
