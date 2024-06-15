b8151ea9f70735b018fb60416f6569d3901d7728
egorovmikhail <egorovmikhail@>
2024-06-15 21:00:42 +0300

    KS-1-4 create commit

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val seconds: Float = 6480f
+
+//    println(seconds / 60)
+//    println(seconds % 60)
+
+    val minute: Float = 108f
+
+//    println(minute / 60)
+//    println(minute % 60)
+
+    val hour: Char = '1'
+
+//    println(hour)
+
+    val flightTime: String = "01:48:00"
+    println(flightTime)
+}
