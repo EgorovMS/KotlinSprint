2e7875c3c80dc0a6e8a2443a5da241ca7dde0134
egorovmikhail <egorovmikhail@>
2024-06-16 13:22:33 +0300

    KS-1-2 overwriting a variable

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -3,12 +3,12 @@ package org.example.lesson_1
 fun main() {
     val numberOforders: Int = 75
     val textOfgratitude: String = "Спасибо за покупку"
-//    val numberOfemployees: Int = 2000
+    var numberOfemployees: Int = 2000
 
     println(numberOforders)
     println(textOfgratitude)
 //    println(numberOfemployees)
 
-    val numberOfemployees: Int = 1999
+    numberOfemployees = 1999
     println(numberOfemployees)
 }
