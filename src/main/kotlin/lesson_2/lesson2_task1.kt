149530b302f63529af04330a70b4eac474a82b31
egorovmikhail <egorovmikhail@>
2024-06-17 21:38:44 +0300

    KS-2-1 average score students

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val student1: Float = 3f
+    val student2: Float = 4f
+    val student3: Float = 3f
+    val student4: Float = 5f
+    val sum = student1 + student2 + student3 + student4
+
+    println(sum / 4)
+}
