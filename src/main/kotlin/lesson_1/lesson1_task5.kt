96787a6db5495bfb2981f396a2bfab522f585395
egorovmikhail <egorovmikhail@>
2024-06-20 10:56:56 +0300

    KS-1-5 adding a constant

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -3,29 +3,15 @@ package org.example.lesson_1
 fun main() {
 
     val timeInSpaceInSeconds: Int = 6480
-    val numberOfSecondsInMinutes: Int = 60
 
-    val secondsInSpace = timeInSpaceInSeconds % numberOfSecondsInMinutes
-    val timeInSpaceInMinutes = timeInSpaceInSeconds / numberOfSecondsInMinutes
+    val secondsInSpace = timeInSpaceInSeconds % TRANSLATE_OF_TIME
+    val timeInSpaceInMinutes = timeInSpaceInSeconds / TRANSLATE_OF_TIME
 
-//    println(secondsInSpace)
-//    println(timeInSpaceInMinutes)
+    val minutesInSpace = timeInSpaceInMinutes % TRANSLATE_OF_TIME
+    val timeInSpaceInHours = timeInSpaceInMinutes / TRANSLATE_OF_TIME
 
-    val numberOfMinutesInHours: Int = 60
-    val minutesInSpace = timeInSpaceInMinutes % numberOfMinutesInHours
-    val timeInSpaceInHours = timeInSpaceInMinutes / numberOfMinutesInHours
+    println(String. format("%02d:%d:%02d", timeInSpaceInHours, minutesInSpace, secondsInSpace))
 
-//    println(minutesInSpace)
-//    println(timeInSpaceInHours)
-
-    println(String. format("%02d:%d:%02d", 1, 48, 0))
-
-
-
-
-//    secondsInSpace
-    val minute: Float = 108f
-
-
-//    println(flightTime)
 }
+
+const val TRANSLATE_OF_TIME = 60
