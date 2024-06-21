29e9578b7360ebfc14a3b08c95db343565217610
egorovmikhail <egorovmikhail@>
2024-06-21 13:02:01 +0300

    KS-3-2 string interpolation

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_3
+
+fun main () {
+
+    val surname: String = "Андреева"
+    val name: String = "Татьяна"
+    val patronymic: String = "Сергеевна"
+
+    val ageBeforeMarriage = 20
+
+    val surnameAfterMarriage: String = "Сидорова"
+
+    val ageAfterMarriage = 22
+
+    println("$surname $name $patronymic, $ageBeforeMarriage")
+    println("$surnameAfterMarriage $name $patronymic, $ageAfterMarriage")
+}
+
+
