21743b3dde3eaa31680fc16a82af45a5e48a7e29
egorovmikhail <egorovmikhail@>
2024-06-21 13:04:33 +0300

    KS-3-3 multiline interpolation

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val number = 7
+    val multiTable = """ 
+        |$number * 1 = ${number * 1}
+        |$number * 2 = ${number * 2}
+        |$number * 3 = ${number * 3}
+        |$number * 4 = ${number * 4}
+        |$number * 5 = ${number * 5}
+        |$number * 6 = ${number * 6}
+        |$number * 7 = ${number * 7}
+        |$number * 8 = ${number * 8}
+        |$number * 9 = ${number * 9}
+        |$number * 10 = ${number * 10}
+""".trimMargin()
+    println(multiTable)
+}
