a54d7d21c1641205bb319d6098c4a19c39d2f724
egorovmikhail <egorovmikhail@>
2024-09-14 19:00:49 +0300

    KS-6-2 remove loops

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -2,14 +2,7 @@ package org.example.lesson_6
 
 fun main() {
     println("Введите количество секунд, которые нужно засечь")
-    val numberOfSeconds = readln().toInt()
-
-    var counter = numberOfSeconds
-
-    while (counter > 0) {
-        println("${counter--}")
-        Thread.sleep(1000)
-        if (counter == 0)
-        println("Прошло $numberOfSeconds секунд")
-    }
+    val numberOfSeconds = readln().toLong()
+    Thread.sleep(numberOfSeconds * 1000)
+    println("Прошло $numberOfSeconds секунд")
 }
