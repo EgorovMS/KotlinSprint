a98b1ceb6c6a35307cec13fac0471d1f30d7b703
egorovmikhail <egorovmikhail@>
2024-09-15 15:52:48 +0300

    KS-6-4 adding the number of iterations

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -5,18 +5,17 @@ fun main() {
     val number = (1..9).random()
     var numberOfAttempts = 5
     var userNumber: Int
+
     println("Угадайте число от 1 до 9. У вас есть ${numberOfAttempts--} попыток")
 
     do {
+        println("Введите число")
         userNumber = readln().toInt()
-        if (number != userNumber) println("Осталось ${numberOfAttempts--} попытки")
-    } while (number != userNumber)
-    println("Это была великолепная игра!")
+        if (number != userNumber) println("Вы не угадали. Осталось ${numberOfAttempts} попытки")
+        if (number == userNumber) {
+            println("Это была великолепная игра!")
+            return
+        }
+    } while (numberOfAttempts-- > 0)
     println("Было загадано число $number")
 }
-
-//(number == userNumber)
-//(number != userNumber)
-//println("Вы не угадали. Осталось ${numberOfAttempts-1} попытки")
-//println("Это была великолепная игра!")
-//(println("Было загадано число $number")
