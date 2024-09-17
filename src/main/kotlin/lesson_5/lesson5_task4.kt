5e2d5425077f3f8ec808cce702167c44fb36abd2
egorovmikhail <egorovmikhail@>
2024-09-17 20:13:18 +0300

    KS-5-4 adding phrases

--- src/main/kotlin/lesson_5/lesson5_task4.kt
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -5,7 +5,18 @@ fun main() {
     val login = "Zaphod"
     val password ="PanGalactic"
 
-    println("Введите логин")
+    val greetings = """
+        |Внимание, пассажир. Моя обязанность, как обычно, незавидная, - приветствовать вас на борту корабля "Heart of Gold". 
+        |Что ж, введи, пожалуйста, свое имя пользователя, чтобы мы могли приступить к процессу входа. 
+        |Я всегда готов служить, хотя это не приносит мне радости... но вы, наверное, об этом не заботитесь.
+        |Да, вперед, пожалуйста, вводите свои данные...""".trimMargin()
+    println(greetings)
+
+    val welcomeMessage = """
+        |Ваши данные проверены, и о, чудо, они верны...
+        |Пользователь "Zaphod", вам разрешено входить на борт корабля "Heart of Gold".
+        |Хотя мне всё равно... Ну вперед, войдите... Если вам так уж надо, в конце концов...
+        |Надеюсь, вам понравится пребывание здесь больше, чем мне.""".trimMargin()
 
     val consoleLogin = readLine()
     if (consoleLogin != login) {
@@ -14,9 +25,9 @@ fun main() {
     } else {
         println("Введите пароль")
     }
-    val consolePassword = readLine()
+    val consolePassword = readln()
     if (consoleLogin == login && consolePassword == password) {
-        println("Доступ к управлению кораблем разрешен.")
+        println(welcomeMessage)
     } else if (consoleLogin == login && consolePassword != password) {
         println("Доступ к управлению кораблем запрещен! Неверный пароль.")
     }
