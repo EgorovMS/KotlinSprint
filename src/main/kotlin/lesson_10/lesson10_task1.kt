b0b5db554749f6692a4439126da2c1bf6d781a55
egorovmikhail <egorovmikhail@>
2024-10-15 23:21:51 +0300

    KS-10-1 dice

---
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_10
+
+fun main() {
+    val throwHuman = dice(player = "Человека")
+    val throwMachine = dice(player = "Машины" )
+    when {
+        throwHuman > throwMachine -> println("Победило человечество")
+        throwHuman < throwMachine -> println("Победила машина")
+    }
+}
+
+fun dice(player: String): Int {
+
+    val diceRoll= (1..6).random()
+    println("Бросок $player $diceRoll")
+    return diceRoll
+}
