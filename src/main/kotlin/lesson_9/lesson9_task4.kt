f2d2a64f7de62498a784484cfdb16ea508174445
egorovmikhail <egorovmikhail@>
2024-10-17 21:53:19 +0300

    KS-9-4 code optimization

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -3,9 +3,6 @@ package org.example.lesson_9
 fun main() {
 
     println("Введите пять ингредиентов, перечисленных через запятую с пробелом: ")
-
-    val inputIngredients: String = readln()
-    val ingredients = inputIngredients.split(", ")
-    val ingredientsList = ingredients.sorted()
-        println("$ingredientsList")
+    val ingredients = readln().split(", ").sorted()
+        println("$ingredients")
 }
